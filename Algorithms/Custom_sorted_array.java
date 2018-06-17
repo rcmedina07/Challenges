abd492952cdc3744ee3a90ad7abd660d3b7c13fa
Rosalba Carralero Medina <rcmedina07@gmail.com>
2018-06-17 12:05:01 -0300

    Create Custom_sorted_array.java

---
+++ Algorithms/Custom_sorted_array.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Custom_sorted_array {
+    public static int moves(int[] a) {
+        int result = 0;
+        int ini = 0;
+        int fin = a.length - 1;
+
+        while (ini < fin) {
+            while ((a[ini] % 2 == 0) && ini < fin) {
+                ini++;
+            }
+
+            while ((a[fin] % 2 != 0) && ini < fin) {
+                fin--;
+            }
+
+            if (ini < fin) {
+                result++;
+                ini++;
+                fin--;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        int[] arr = new int[t];
+        for (int a0 = 0; a0 < t; a0++) {
+            int elem = in.nextInt();
+            arr[a0] = elem;
+        }
+        System.out.println(moves(arr));
+
+    }
+
+}
